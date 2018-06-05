95dd8f50fa583552c65d1f804e8b288bfe573db0
yuika.tomiyasu <yuika.tomiyasu@rakuten.com>
2018-06-05 06:35:15 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,4 +1,24 @@
- public function show($id)
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User; // 追加
+use App\Micropost; // add
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(10);
+        
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+    
+     public function show($id)
     {
         $user = User::find($id);
         $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
@@ -12,3 +32,33 @@
 
         return view('users.show', $data);
     }
+    public function followings($id)
+    {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followings', $data);
+    }
+
+    public function followers($id)
+    {
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followers', $data);
+    }
+}
